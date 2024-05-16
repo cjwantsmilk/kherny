3f84f0584888f0cff2346e35869d0e3b4518e04c
cjwantsmilk <147643494+cjwantsmilk@users.noreply.github.com>
2024-05-16 11:23:46 -0600

    Update iterativehead.cpp

--- iterativehead.cpp
+++ iterativehead.cpp
@@ -2,37 +2,34 @@
 #include "karyheads.cpp"
 #include <iostream>
 
-void iterativeHeapSort(int rap[], int size) {
-    KaryHeap heap(2);
-    for (int i    = 0; i < size; ++i)
+void iterativeheaps(int rap[], int size) {
+    KaryHeap  heap(2);
+    for (int  i    = 0; i < size; ++i)
         heap.insert(rap[i]);
-    for (int i = 0; i < size; ++i)
-        rap[i] =   heap.deleteMin();
+    for (int i =  0; i < size; ++i)
+        rap[i] =    heap.deleteMin();
 int main() {
-    int rap[] = {5, 1, 10, 4, 7};
+    int rap[] = {5,  1, 10, 4, 7};
     int size = sizeof(rap) / sizeof(rap[0]);
     int rap1[] = {12, 8, 9, 21, 2};
     int size1 = sizeof(rap1) / sizeof(rap1[0]);
-    int rapgpp[] = {11, 56, 32, 20, 9, 1, 16};
-    int size2 = sizeof(rapgpp) / sizeof(rapgpp[0]);
-    iterativeHeapSort(rap, size);
+    int rapgpp[] = {11, 56,  32, 20, 9, 1, 16};
+    int size2 = sizeof(rapgpp)  / sizeof(rapgpp[0]);
+    iterativeheaps(rap, size);
 
-    std::cout << "sorting the rapay ---> ";
+    std::cout   << "sorting the rapay ---> ";
     for (int i = 0; i < size; ++i)
            std::cout << rap[i] << " ";
-    std::cout << std::endl;
-    iterativeHeapSort(rap1, size1);
-
-    std::cout << "Sorted rapay: ";
+    std::cout   << std::endl;
+    iterativeheaps(rap1, size1);
+    std::cout   << "sorted repay ---> ";
     for (int i = 0; i < size; ++i)
-        std::cout << rap1[i] << " ";
+         std::cout << rap1[i] << " ";
     std::cout << std::endl;
-
-    iterativeHeapSort(rapgpp, size2);
-
+    iterativeheaps(rapgpp, size2);
     std::cout << "sorted rapay -->  ";
-    for (int i = 0; i < size; ++i)
+    for   (int i = 0; i < size; ++i)
         std::cout << rapgpp[i] << " ";
-    std::cout << std::endl;
+    std::cout    << std::endl;
 
 }
