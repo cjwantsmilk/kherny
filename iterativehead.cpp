778cf17dd17a3024acc931c7540b225b15f8760b
cjwantsmilk <147643494+cjwantsmilk@users.noreply.github.com>
2024-05-16 11:22:27 -0600

    Update iterativehead.cpp

--- iterativehead.cpp
+++ iterativehead.cpp
@@ -4,13 +4,12 @@
 
 void iterativeHeapSort(int rap[], int size) {
     KaryHeap heap(2);
-    for (int i = 0; i < size; ++i)
+    for (int i    = 0; i < size; ++i)
         heap.insert(rap[i]);
     for (int i = 0; i < size; ++i)
-        rap[i] = heap.deleteMin();
-
+        rap[i] =   heap.deleteMin();
 int main() {
-    int rap[] = {5, 1, 9, 4, 7};
+    int rap[] = {5, 1, 10, 4, 7};
     int size = sizeof(rap) / sizeof(rap[0]);
     int rap1[] = {12, 8, 9, 21, 2};
     int size1 = sizeof(rap1) / sizeof(rap1[0]);
@@ -18,13 +17,11 @@ int main() {
     int size2 = sizeof(rapgpp) / sizeof(rapgpp[0]);
     iterativeHeapSort(rap, size);
 
-    std::cout << "Sorted rapay: ";
+    std::cout << "sorting the rapay ---> ";
     for (int i = 0; i < size; ++i)
-        std::cout << rap[i] << " ";
+           std::cout << rap[i] << " ";
     std::cout << std::endl;
-
     iterativeHeapSort(rap1, size1);
-
 
     std::cout << "Sorted rapay: ";
     for (int i = 0; i < size; ++i)
