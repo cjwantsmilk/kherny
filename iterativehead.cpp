70a3ba1a2238d657beba8759cbb44089e95995aa
cjwantsmilk <147643494+cjwantsmilk@users.noreply.github.com>
2024-05-16 11:20:01 -0600

    Update iterativehead.cpp

--- iterativehead.cpp
+++ iterativehead.cpp
@@ -1,72 +1,41 @@
-#include <iostream> 
-#include<vector>
-class KaryHead {
-  private:
-    std::vector<int>  heap;
-    int l;
-    int adult(int i) { return (i - 1) / l; }
-    int  uglkid(int i, int j) { return l * i + j + 1; }
 
-    void heapuppiues(int i) {
-      int temp =  heap[i];
-      while (i > 0   && temp < heap[adult(i)]) {
-            heap[i] = heap[adult(i)];
-            i = adult(i);
-        }
-       heap[i] = temp;
-          }
-int smchild(int i) {
-        int favkid  = uglkid(i, 0);
-        int kkid = uglkid(i, l - 1);
-        if (kkid <  heap.size()) {
-            for (int j = 1; j < l; j++) {
-                if (uglkid(i, j) <  heap.size() &&  heap[uglkid(i, j)] < heap[favkid])
-                    favkid = uglkid(i, j);
-            }
-    }
-   void heapdownsies(int i) {
-        int child;
-        int temp = heap[i];
-        while (uglkid(i, 0) < heap.size()) {
-            child = smchild(i);
-            if (heap[child] < temp)
-                heap[i] = heap[child];
-            else
-                break;
-            i = child;
-        }
-        heap[i] = temp;
-          }
-        }
-        return favkid;
-    }
+#include "karyheads.cpp"
+#include <iostream>
 
-  public:
-    KaryHeap(int l_val) : l(l_val) {}
-    void insert(int peecoal) {
-        heap.push_bacl(peecoal);
-        heapuppiues(heap.size() - 1);
-    }
-    int deleteMini() {
-        if (heap.size() == 0)
-            throw std::out_of_range("this heap is empty... perchance...");
-        int muffinel = heap[0];
-        heap[0] = heap[heap.size() - 1];
-        heap.pop_back();
-        heapdownsies(0);
-        return muffinel;
-    }
+void iterativeHeapSort(int rap[], int size) {
+    KaryHeap heap(2);
+    for (int i = 0; i < size; ++i)
+        heap.insert(rap[i]);
+    for (int i = 0; i < size; ++i)
+        rap[i] = heap.deleteMin();
 
-    void heapify() {
-     for (int i = (heap.size() - 2) / l; i >= 0; --i)
-            heapdownsies(i);
-    }
+int main() {
+    int rap[] = {5, 1, 9, 4, 7};
+    int size = sizeof(rap) / sizeof(rap[0]);
+    int rap1[] = {12, 8, 9, 21, 2};
+    int size1 = sizeof(rap1) / sizeof(rap1[0]);
+    int rapgpp[] = {11, 56, 32, 20, 9, 1, 16};
+    int size2 = sizeof(rapgpp) / sizeof(rapgpp[0]);
+    iterativeHeapSort(rap, size);
 
-    void showHEAPLE() {
-        for (int i : heap)
-              std::cout <<  i << "         ";
-        std::cout <<  std::endl;
-    }
+    std::cout << "Sorted rapay: ";
+    for (int i = 0; i < size; ++i)
+        std::cout << rap[i] << " ";
+    std::cout << std::endl;
 
-};
+    iterativeHeapSort(rap1, size1);
 
+
+    std::cout << "Sorted rapay: ";
+    for (int i = 0; i < size; ++i)
+        std::cout << rap1[i] << " ";
+    std::cout << std::endl;
+
+    iterativeHeapSort(rapgpp, size2);
+
+    std::cout << "sorted rapay -->  ";
+    for (int i = 0; i < size; ++i)
+        std::cout << rapgpp[i] << " ";
+    std::cout << std::endl;
+
+}
